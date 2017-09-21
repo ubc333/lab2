ab3d9ee2d001656a1cf1dad73521c1c7be8fe6dd
Antonio Sanchez <antonios@ece.ubc.ca>
2017-09-21 07:55:16 -0700

    added Function example definition and use

--- src/integration.cpp
+++ src/integration.cpp
@@ -44,7 +44,30 @@ class ZFunction : public Function {
   }
 };
 
+// new function for representing density 1
+class Density1 : public Function {
+ public:
+  double operator()(double x, double y, double z) {
+    double norm2 = x*x+y*y+z*z;
+    if (norm2 > 1) {
+      return 0;
+    }
+    return std::exp(-norm2);
+  }
+};
+
 int main() {
+
+  // sample usage of functions
+  Density1 d1;
+  XFunction xd1(d1);  // x*d1(x,y,z)
+  YFunction yd1(d1);  // y*d1(x,y,z)
+  ZFunction zd1(d1);  // z*d1(x,y,z)
+
+  std::cout << "d1(0.1,0.2,0.3): " << d1(0.1,0.2,0.3) << std::endl;
+  std::cout << "xd1(0.1,0.2,0.3): " << xd1(0.1,0.2,0.3) << std::endl;
+  std::cout << "yd1(0.1,0.2,0.3): " << yd1(0.1,0.2,0.3) << std::endl;
+  std::cout << "zd1(0.1,0.2,0.3): " << zd1(0.1,0.2,0.3) << std::endl;
 
   // YOUR CODE HERE
 
