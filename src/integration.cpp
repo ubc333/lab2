37a56241d2827e4c083a5404281982c7b6927c29
Antonio Sanchez <antonios@ece.ubc.ca>
2017-09-26 15:02:04 -0700

    operator to member function

--- src/integration.cpp
+++ src/integration.cpp
@@ -11,7 +11,7 @@ struct Point {
 // virtual base class for functions
 class Function {
  public:
-  virtual double operator()(double x, double y, double z) = 0;
+  virtual double eval(double x, double y, double z) = 0;
 };
 
 // computes x*fn(x,y,z)
@@ -19,8 +19,8 @@ class XFunction : public Function {
   Function& fn;
  public:
   XFunction(Function& fn) : fn(fn){};
-  double operator()(double x, double y, double z) {
-    return x*fn(x,y,z);
+  double eval(double x, double y, double z) {
+    return x*fn.eval(x,y,z);
   }
 };
 
@@ -29,8 +29,8 @@ class YFunction : public Function {
   Function& fn;
  public:
   YFunction(Function& fn) : fn(fn){};
-  double operator()(double x, double y, double z) {
-    return y*fn(x,y,z);
+  double eval(double x, double y, double z) {
+    return y*fn.eval(x,y,z);
   }
 };
 
@@ -39,15 +39,15 @@ class ZFunction : public Function {
   Function& fn;
  public:
   ZFunction(Function& fn) : fn(fn){};
-  double operator()(double x, double y, double z) {
-    return z*fn(x,y,z);
+  double eval(double x, double y, double z) {
+    return z*fn.eval(x,y,z);
   }
 };
 
 // new function for representing density 1
 class Density1 : public Function {
  public:
-  double operator()(double x, double y, double z) {
+  double eval(double x, double y, double z) {
     double norm2 = x*x+y*y+z*z;
     if (norm2 > 1) {
       return 0;
@@ -64,10 +64,10 @@ int main() {
   YFunction yd1(d1);  // y*d1(x,y,z)
   ZFunction zd1(d1);  // z*d1(x,y,z)
 
-  std::cout << "d1(0.1,0.2,0.3): " << d1(0.1,0.2,0.3) << std::endl;
-  std::cout << "xd1(0.1,0.2,0.3): " << xd1(0.1,0.2,0.3) << std::endl;
-  std::cout << "yd1(0.1,0.2,0.3): " << yd1(0.1,0.2,0.3) << std::endl;
-  std::cout << "zd1(0.1,0.2,0.3): " << zd1(0.1,0.2,0.3) << std::endl;
+  std::cout << "d1.eval(0.1,0.2,0.3): " << d1.eval(0.1,0.2,0.3) << std::endl;
+  std::cout << "xd1.eval(0.1,0.2,0.3): " << xd1.eval(0.1,0.2,0.3) << std::endl;
+  std::cout << "yd1.eval(0.1,0.2,0.3): " << yd1.eval(0.1,0.2,0.3) << std::endl;
+  std::cout << "zd1.eval(0.1,0.2,0.3): " << zd1.eval(0.1,0.2,0.3) << std::endl;
 
   // YOUR CODE HERE
 
